5266f538061ff94e0ad70cabff8782b1a90cfd50
Caleb Gomes Pinto <caleb.gp.dev@gmail.com>
2024-05-08 11:16:41 +0000

    fix: improve if condition of 2010´s C exercise

--- exercises-2010/b.py
+++ exercises-2010/b.py
@@ -1,15 +1,11 @@
 def rock_paper_scissors(p1, p2):
-    if p1 == 'R' and p2 == 'T':
+    if (p1 == 'R' and p2 == 'T') or (p1 == 'P'
+                                     and p2 == 'R') or (p1 == 'T'
+                                                        and p2 == 'P'):
         return 1
-    elif p1 == 'P' and p2 == 'R':
-        return 1
-    elif p1 == 'T' and p2 == 'P':
-        return 1
-    elif p2 == 'R' and p1 == 'T':
-        return 2
-    elif p2 == 'P' and p1 == 'R':
-        return 2
-    elif p2 == 'T' and p1 == 'P':
+    elif (p2 == 'R' and p1 == 'T') or (p2 == 'P'
+                                       and p1 == 'R') or (p2 == 'T'
+                                                          and p1 == 'P'):
         return 2
     else:
         return 0
@@ -17,7 +13,7 @@ def rock_paper_scissors(p1, p2):
 
 def play(op1, op2):
     wins = {}
-    for i in range(10):
+    for i in range(len(op1)):
         winner = rock_paper_scissors(op1[i], op2[i])
         if winner != 0:
             wins[winner] = wins.get(winner, 0) + 1
